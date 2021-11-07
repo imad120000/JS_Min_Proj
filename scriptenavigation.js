150045a0f61885ab3187d15704599a8e1ac76dcf
imad120000 <90506879+imad120000@users.noreply.github.com>
2021-11-07 02:12:59 +0100

    Add files via upload

--- scriptenavigation.js
+++ scriptenavigation.js
@@ -1,34 +1,80 @@
 function a1(){
-    var z = document.createElement("div");
-    z.innerHTML="hello it's been long temps brother so wake up...";
-    document.getElementById("j").appendChild(z);
-    z.setAttribute("style","background:grenn");
-    document.getElementById("l").innerHTML="";
-    document.getElementById("m").innerHTML="";
+    //first Methode Create element
+
+    /*var z = document.createElement("div");
+    z.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget maximus ipsum. Proin mollis neque a nulla egestas, eu blandit metus facilisis. Praesent tincidunt<br> nisl tellus, at malesuada mi mollis ac. Phasellus iaculis vulputate libero, at iaculis ante porttitor nec. Fusce id malesuada enim, sit amet tristique purus. Praesent ultricies sollicitudin convallis. Sed dictum est nunc. Ut ultrices elit sed ligula molestie, vel pharetra sem tincidunt. Proin vitae<br> pellentesque dui. Donec vitae tristique odio, tempor bibendum mauris. Nunc ultricies accumsan auctor. Quisque maximus maximus purus ac fermentum. Suspendisse ac lorem metus. Nam finibus justo quis vestibulum cursus. Curabitur dictum ultricies tempor.Aliquam porta nisl <br>eget eros porta fermentum. Praesent non dui quis risus convallis blandit eget quis quam. Curabitur mauris justo, convallis ultricies dolor vestibulum, facilisis sollicitudin libero. Cras tempor, orci quis volutpat<br> luctus, nunc eros lobortis nisi, eget mattis arcu mi vitae nulla. Vestibulum eu lectus ut nisi ullamcorper molestie porta sit amet ipsum. Praesent elit felis, iaculis nec pulvinar ac, ultricies eget dui. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus<br> orci luctus et ultrices posuere cubilia curae; Nullam consectetur elementum sapien in pulvinar. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In hac habitasse platea dictumst. Phasellus dapibus velit pellentesque, scelerisque nunc ut, feugiat magna. Proin vel urna mattis,<br> scelerisque nulla vitae, elementum justo. Quisque nulla est, dignissim in urna vitae, viverra dapibus lectus.<br> Proin non maximus ante, a elementum felis. Vivamus in semper sapien."
+    document.getElementById("z").appendChild(z);
+    z.setAttribute("style","background:green");
+    document.getElementById("w").innerHTML="";
+    document.getElementById("y").innerHTML="";
+    var e = document.getElementById("p");
+    e.setAttribute("style","background:green");*/
+
+    //secand Methode
+
+    document.getElementById("t3").setAttribute("style","background-color: cadetblue;");
+    document.getElementById("t2").setAttribute("style","background-color: cadetblue;");
+    document.getElementById("y").style.display="none";
+    document.getElementById("w").style.display="none";
+    var a = document.getElementById("z");
+    a.style.display="block";
+    document.getElementById("t1").setAttribute("style","background-color: lime;");
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 function a2(){
-    var z = document.createElement("div");
+    //first Methode Create element
+  /*  var z = document.createElement("div");
     z.innerHTML="<h1>History</h1><p>Thecepts, sucresequent sections are arranged chronologically, covering the Early Byzantine period (c. 330–700), the Iconoclastic Controversy (c. 700s–843), the Middle Byzantine period (843–1204), the Latin Empire (c. 1204–1261), and the Late Byzantine period (c. 1261–1453) and beyond This book has been a part of my pandemic journey with a goal of building English language learner resources, gathering up what I have learned about anti-racist, culturally responsive, and decolonization approaches. I know that I have not nearly met this goal in this single resource and that there is so much more to do. I am simply starting on the collective path and am so humbled to join fellow colleagues in the work of rewriting the myths and false narratives of our field. This goes well beyond one specific discipline. It is a call to all educators and all institutions to choose love in action, to choose change.</p>";
-    document.getElementById("l").appendChild(z);
+    document.getElementById("w").appendChild(z);
     z.setAttribute("style","background:red");
-    document.getElementById("j").innerHTML="";
-    document.getElementById("m").innerHTML="";
+    document.getElementById("z").innerHTML="";
+    document.getElementById("y").innerHTML="";
+    var e = document.getElementById("c");
+    e.setAttribute("style","background:red");*/
     //document.getElementById("j").style.display="none";
     //document.getElementById("m").style.display="none";
     //var c = document.getElementById("l");
     //c.style.display="inline";
     //DOM 100% Creation element........
+    document.getElementById("t1").setAttribute("style","background-color: cadetblue;");
+    document.getElementById("t3").setAttribute("style","background-color: cadetblue;");
+    document.getElementById("w").style.display="none";
+    document.getElementById("z").style.display="none";
+    var b = document.getElementById("y");
+    b.style.display="block";
+    document.getElementById("t2").setAttribute("style","background-color: rgb(21, 214, 147);")
+    
+
+
+
+
+
+
 
 }
 function a3(){
-    var z = document.createElement("div");
-    z.innerHTML="hello it's been long temps brother so wake up...";
-    document.getElementById("m").appendChild(z);
-    z.setAttribute("style","background:yellow");
-    document.getElementById("l").innerHTML="";
-    document.getElementById("j").innerHTML="";
+    document.getElementById("t1").setAttribute("style","background-color: cadetblue;");
+    document.getElementById("t2").setAttribute("style","background-color: cadetblue;");
+    document.getElementById("y").style.display="none";
+    document.getElementById("z").style.display="none";
+    var b = document.getElementById("w");
+    b.style.display="block";
+    document.getElementById("t3").setAttribute("style","background-color: greenyellow")
     
 
 }
